a5cf1639b66c1a1780732ecb0cb0ce0b028f3ca0
spacebear <144076611+grizznaut@users.noreply.github.com>
2023-08-10 16:15:41 -0400

    step9
    Signed-off-by: spacebear <144076611+grizznaut@users.noreply.github.com>

--- impls/rust.me/src/core.rs
+++ impls/rust.me/src/core.rs
@@ -2,7 +2,7 @@ use crate::errors::MalErr;
 use crate::printer::pr_list;
 use crate::reader::read_str;
 use crate::types::{atom, func, MalType};
-use crate::{list, vector};
+use crate::{hashmap, list, vector};
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
@@ -61,10 +61,93 @@ fn is_variant(value: &MalType, variant: &str) -> Result<MalType, MalErr> {
     let is_type = match (value, variant) {
         (MalType::List(..), "list") => true,
         (MalType::Atom(..), "atom") => true,
+        (MalType::Nil, "nil") => true,
+        (MalType::Bool(true), "true") => true,
+        (MalType::Bool(false), "false") => true,
+        (MalType::Symbol(..), "symbol") => true,
+        (MalType::Str(s), "keyword") if s.starts_with(KEYWORD_PREFIX) => true,
+        (MalType::Vector(..), "vector") => true,
+        (MalType::List(..) | MalType::Vector(..), "sequential") => true,
+        (MalType::HashMap(..), "hashmap") => true,
         (MalType::List(l, _) | MalType::Vector(l, _), "empty") => l.is_empty(),
         _ => false,
     };
     Ok(MalType::Bool(is_type))
+}
+
+fn symbol(value: &MalType) -> Result<MalType, MalErr> {
+    match value {
+        MalType::Str(s) => Ok(MalType::Symbol(s.to_string())),
+        _ => Err(MalErr::FunctionErr("Expected a string".to_string())),
+    }
+}
+
+fn keyword(value: &MalType) -> Result<MalType, MalErr> {
+    match value {
+        MalType::Str(s) if s.starts_with(KEYWORD_PREFIX) => Ok(value.clone()),
+        MalType::Str(s) => Ok(MalType::Str(KEYWORD_PREFIX.to_owned() + s)),
+        _ => Err(MalErr::FunctionErr("Expected a string".to_string())),
+    }
+}
+
+fn contains(value: &MalType, key: &MalType) -> Result<MalType, MalErr> {
+    match value {
+        MalType::HashMap(hm, _) => Ok(MalType::Bool(hm.contains_key(key))),
+        _ => Err(MalErr::FunctionErr("Expected a hash-map".to_string())),
+    }
+}
+
+fn get(value: &MalType, key: &MalType) -> Result<MalType, MalErr> {
+    match value {
+        MalType::HashMap(hm, _) => Ok(hm.get(key).unwrap_or(&MalType::Nil).clone()),
+        MalType::Nil => Ok(MalType::Nil),
+        _ => Err(MalErr::FunctionErr("Expected a hash-map".to_string())),
+    }
+}
+
+fn keys(value: &MalType) -> Result<MalType, MalErr> {
+    match value {
+        MalType::HashMap(hm, _) => Ok(list!(hm.keys().map(|k| k.clone()).collect())),
+        _ => Err(MalErr::FunctionErr("Expected a hash-map".to_string())),
+    }
+}
+
+fn vals(value: &MalType) -> Result<MalType, MalErr> {
+    match value {
+        MalType::HashMap(hm, _) => Ok(list!(hm.values().map(|v| v.clone()).collect())),
+        _ => Err(MalErr::FunctionErr("Expected a hash-map".to_string())),
+    }
+}
+
+fn assoc(args: Vec<MalType>) -> Result<MalType, MalErr> {
+    match &args[0] {
+        MalType::HashMap(hm, _) => {
+            let mut kvs: Vec<MalType> = (**hm)
+                .clone()
+                .into_iter()
+                .flat_map(|(k, v)| vec![k, v])
+                .collect();
+            kvs.extend_from_slice(&args[1..]);
+            hashmap!(kvs)
+        }
+        _ => Err(MalErr::FunctionErr("Expected a hash-map".to_string())),
+    }
+}
+
+fn dissoc(args: Vec<MalType>) -> Result<MalType, MalErr> {
+    match &args[0] {
+        MalType::HashMap(hm, _) => {
+            let mut new_hm = (**hm).clone();
+            for key in &args[1..] {
+                new_hm.remove(key);
+            }
+            Ok(MalType::HashMap(
+                std::rc::Rc::new(new_hm),
+                std::rc::Rc::new(MalType::Nil),
+            ))
+        }
+        _ => Err(MalErr::FunctionErr("Expected a hash-map".to_string())),
+    }
 }
 
 fn deref(atom: &MalType) -> Result<MalType, MalErr> {
@@ -166,6 +249,33 @@ fn rest(list: &MalType) -> Result<MalType, MalErr> {
     }
 }
 
+fn apply(args: Vec<MalType>) -> Result<MalType, MalErr> {
+    let mut fargs = args.iter();
+    let (f, list) = (fargs.nth(0), fargs.nth_back(0)); // consumes the first and last iter items
+    match list {
+        Some(MalType::List(l, _)) | Some(MalType::Vector(l, _)) => {
+            let mut v: Vec<MalType> = fargs.cloned().collect();
+            v.extend_from_slice(l);
+            f.unwrap().apply(v)
+        }
+        _ => Err(MalErr::FunctionErr("Expected a list of args".to_string())),
+    }
+}
+
+fn map(args: Vec<MalType>) -> Result<MalType, MalErr> {
+    let f = &args[0];
+    match &args[1] {
+        MalType::List(l, _) | MalType::Vector(l, _) => {
+            let map_results = l.iter().map(|el| f.apply(vec![el.clone()])).collect();
+            match map_results {
+                Ok(m) => Ok(list!(m)),
+                Err(e) => Err(e),
+            }
+        }
+        _ => Err(MalErr::FunctionErr("Expected a list of args".to_string())),
+    }
+}
+
 pub fn ns() -> HashMap<&'static str, MalType> {
     let mut ns = HashMap::new();
     ns.insert("+", func(|a| accumulate(a, |x, y| x + y)));
@@ -186,6 +296,24 @@ pub fn ns() -> HashMap<&'static str, MalType> {
     ns.insert("list", func(|a| Ok(list!(a))));
     ns.insert("list?", func(|a| is_variant(&a[0], "list")));
     ns.insert("empty?", func(|a| is_variant(&a[0], "empty")));
+    ns.insert("nil?", func(|a| is_variant(&a[0], "nil")));
+    ns.insert("true?", func(|a| is_variant(&a[0], "true")));
+    ns.insert("false?", func(|a| is_variant(&a[0], "false")));
+    ns.insert("symbol", func(|a| symbol(&a[0])));
+    ns.insert("symbol?", func(|a| is_variant(&a[0], "symbol")));
+    ns.insert("keyword", func(|a| keyword(&a[0])));
+    ns.insert("keyword?", func(|a| is_variant(&a[0], "keyword")));
+    ns.insert("vector", func(|a| Ok(vector!(a))));
+    ns.insert("vector?", func(|a| is_variant(&a[0], "vector")));
+    ns.insert("sequential?", func(|a| is_variant(&a[0], "sequential")));
+    ns.insert("hash-map", func(|a| hashmap!(a)));
+    ns.insert("map?", func(|a| is_variant(&a[0], "hashmap")));
+    ns.insert("contains?", func(|a| contains(&a[0], &a[1])));
+    ns.insert("get", func(|a| get(&a[0], &a[1])));
+    ns.insert("keys", func(|a| keys(&a[0])));
+    ns.insert("vals", func(|a| vals(&a[0])));
+    ns.insert("assoc", func(|a| assoc(a)));
+    ns.insert("dissoc", func(|a| dissoc(a)));
     ns.insert(
         "count",
         func(|a| match &a[0] {
@@ -207,5 +335,8 @@ pub fn ns() -> HashMap<&'static str, MalType> {
     ns.insert("nth", func(|a| nth(&a[0], &a[1])));
     ns.insert("first", func(|a| first(&a[0])));
     ns.insert("rest", func(|a| rest(&a[0])));
+    ns.insert("throw", func(|a| Err(MalErr::Throw(a[0].clone()))));
+    ns.insert("apply", func(|a| apply(a)));
+    ns.insert("map", func(|a| map(a)));
     ns
 }
--- impls/rust.me/src/errors.rs
+++ impls/rust.me/src/errors.rs
@@ -1,3 +1,5 @@
+use crate::types::MalType;
+
 #[derive(Debug)]
 pub enum MalErr {
     // read
@@ -9,6 +11,7 @@ pub enum MalErr {
     InvalidDo(String),
     FunctionErr(String),
     MalFunctionErr(String),
+    Throw(MalType),
     Generic(String),
 }
 
@@ -16,11 +19,12 @@ impl std::fmt::Display for MalErr {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
             MalErr::ReadErr(message) => write!(f, "Read error: {}", message),
-            MalErr::SymbolNotFound(symbol) => write!(f, "{} not found", symbol),
+            MalErr::SymbolNotFound(symbol) => write!(f, "'{}' not found", symbol),
             MalErr::InvalidLet(message) => write!(f, "Invalid let* construction: {}", message),
             MalErr::InvalidDo(message) => write!(f, "Invalid do construction: {}", message),
             MalErr::FunctionErr(message) => write!(f, "Does not compute: {}", message),
             MalErr::MalFunctionErr(message) => write!(f, "Invalid fn* construction: {}", message),
+            MalErr::Throw(mt) => write!(f, "{}", mt.pr_str(false)),
             MalErr::Generic(message) => write!(f, "Error: {}", message),
         }
     }
